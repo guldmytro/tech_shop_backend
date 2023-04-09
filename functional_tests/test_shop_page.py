82010837e0c1a42f5c31ae21c38371f584868227
Dmytro <guldmytro@gmail.com>
2023-04-09 10:13:43 +0300

    wrote simple test of single product page

--- functional_tests/test_shop_page.py
+++ functional_tests/test_shop_page.py
@@ -1,9 +1,25 @@
 from .base import FunctionalTest
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+import time
+
+
+MAX_WAIT = 10
 
 
 class MyShopTest(FunctionalTest):
     """тест страницы магазина"""
+
+    def wait_for(self, fn):
+        """ожидать"""
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_go_to_the_shop_page(self):
         """тест: пользователь может перейти на страницу магазина"""
@@ -15,7 +31,30 @@ class MyShopTest(FunctionalTest):
         self.browser.find_element(By.LINK_TEXT, 'Магазин').click()
 
         # Там она находит заголовок с надписью "Каталог товарів"
-        title = self.browser.find_element(By.CSS_SELECTOR,
-                                          'h1.section-header__title').text
-        self.assertEqual('Каталог товарів', title)
+        title = self.wait_for(
+            lambda: self.browser.find_element(By.CSS_SELECTOR,
+                                          'h1.section-header__title'))
+        self.assertEqual('Каталог товарів', title.text)
+
+    def test_can_visit_singe_product_page(self):
+        """тест: можно перейти на страницу пользователя"""
+
+        # Эдит переходит на страницу магазина
+        self.browser.get(f'{self.live_server_url}/shop')
+
+        # Там она находит товар который её интересует и переходит на его
+        # страницу
+        product_link = self.wait_for(
+            lambda: self.browser.find_element(By.CSS_SELECTOR,
+                                                 '.product-item__title a'))
+        product_link_text = product_link.text
+        product_link.click()
+
+        # Там заголовок товара соответствует
+        single_product_title = self.wait_for(
+            lambda: self.browser.find_element(By.CSS_SELECTOR,
+                                              '.product-info__title')
+        )
+        self.assertEqual(product_link_text, single_product_title.text)
+
 
